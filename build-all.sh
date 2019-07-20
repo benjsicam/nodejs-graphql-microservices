9d30c2dfa6ac0a48cd947ff7316c1eaef09ff972
Benj Sicam <benj.sicam@mi-c3.com>
2019-07-20 13:59:01 +0800

    Added npm install on build

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd graphql-gateway && npm run copy:protos && npm run build && cd -
-cd grpc-services/comments && npm run copy:protos && npm run build && cd -
-cd grpc-services/posts && npm run copy:protos && npm run build && cd -
-cd grpc-services/users && npm run copy:protos && npm run build && cd -
+cd graphql-gateway && npm i && npm run copy:protos && npm run build && cd -
+cd grpc-services/comments && npm i && npm run copy:protos && npm run build && cd -
+cd grpc-services/posts && npm i && npm run copy:protos && npm run build && cd -
+cd grpc-services/users && npm i && npm run copy:protos && npm run build && cd -
