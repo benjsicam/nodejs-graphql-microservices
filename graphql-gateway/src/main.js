7685ce45cef2722657da13f12a4772440c6439a5
Benj Sicam <benj.sicam@mi-c3.com>
2019-07-21 17:05:30 +0800

    Minor enhancements

--- graphql-gateway/src/main.js
+++ graphql-gateway/src/main.js
@@ -1,9 +1,6 @@
 import path from 'path'
-import pino from 'pino'
 import Redis from 'ioredis'
 
-import { GraphQLServer } from 'graphql-yoga'
-import { assign, reduce, startCase } from 'lodash'
 import { RedisPubSub } from 'graphql-redis-subscriptions'
 
 import * as QueryResolvers from 'glob:./resolvers/**/*.query.js' // eslint-disable-line
@@ -11,78 +8,52 @@ import * as MutationResolvers from 'glob:./resolvers/**/*.mutation.js' // eslint
 import * as SubscriptionResolvers from 'glob:./resolvers/**/*.subscription.js' // eslint-disable-line
 import * as GraphResolvers from 'glob:./resolvers/**/*.graph.js' // eslint-disable-line
 
+import logger from './logger'
+import Server from './server'
 import ServiceIndex from './services/_index'
 import defaultPlaygroundQuery from './default-query'
 
-const logger = pino({
-  safe: true,
-  prettyPrint: process.env.NODE_ENV === 'dev'
-})
+const SCHEMA_PATH = path.resolve(__dirname, 'schema.graphql')
 
-const serviceIndex = new ServiceIndex(logger)
+async function main() {
+  const serviceIndex = new ServiceIndex(logger)
 
-const redisConnOpts = {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  keyPrefix: process.env.NODE_ENV
+  const redisConnOpts = {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    keyPrefix: process.env.NODE_ENV
+  }
+
+  const pubsub = new RedisPubSub({
+    connection: redisConnOpts,
+    publisher: new Redis(redisConnOpts),
+    subscriber: new Redis(redisConnOpts)
+  })
+
+  const server = await Server.init(
+    SCHEMA_PATH,
+    {
+      QueryResolvers,
+      MutationResolvers,
+      SubscriptionResolvers,
+      GraphResolvers
+    },
+    {
+      ...serviceIndex.services,
+      pubsub,
+      logger
+    }
+  )
+
+  server.start(
+    {
+      defaultPlaygroundQuery,
+      port: process.env.PORT || 3000
+    },
+    ({ port }) => {
+      logger.info(`GraphQL Server is now running on port ${port}`)
+    }
+  )
 }
 
-const pubsub = new RedisPubSub({
-  connection: redisConnOpts,
-  publisher: new Redis(redisConnOpts),
-  subscriber: new Redis(redisConnOpts)
-})
-
-const server = new GraphQLServer({
-  typeDefs: path.resolve(__dirname, 'schema.graphql'),
-  resolvers: {
-    Query: reduce(
-      QueryResolvers,
-      (res, val) => {
-        return assign(res, val)
-      },
-      {}
-    ),
-    Mutation: reduce(
-      MutationResolvers,
-      (res, val) => {
-        return assign(res, val)
-      },
-      {}
-    ),
-    Subscription: reduce(
-      SubscriptionResolvers,
-      (res, val) => {
-        return assign(res, val)
-      },
-      {}
-    ),
-    ...reduce(
-      GraphResolvers,
-      (res, val, key) => {
-        const obj = {}
-        obj[startCase(key.substr(key.lastIndexOf('$') + 1))] = val
-        return assign(res, obj)
-      },
-      {}
-    )
-  },
-  context(req) {
-    return {
-      ...req,
-      ...serviceIndex.services,
-      logger,
-      pubsub
-    }
-  }
-})
-
-server.start(
-  {
-    defaultPlaygroundQuery,
-    port: process.env.PORT || 3000
-  },
-  ({ port }) => {
-    logger.info(`GraphQL Server is now running on port ${port}`)
-  }
-)
+main()
---
+++ graphql-gateway/src/server.js
@@ -0,0 +1,52 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { assign, reduce, startCase } from 'lodash'
+
+const Server = {
+  async init(schemaPath, resolvers, services) {
+    const server = new GraphQLServer({
+      typeDefs: schemaPath,
+      resolvers: {
+        Query: reduce(
+          resolvers.QueryResolvers,
+          (res, val) => {
+            return assign(res, val)
+          },
+          {}
+        ),
+        Mutation: reduce(
+          resolvers.MutationResolvers,
+          (res, val) => {
+            return assign(res, val)
+          },
+          {}
+        ),
+        Subscription: reduce(
+          resolvers.SubscriptionResolvers,
+          (res, val) => {
+            return assign(res, val)
+          },
+          {}
+        ),
+        ...reduce(
+          resolvers.GraphResolvers,
+          (res, val, key) => {
+            const obj = {}
+            obj[startCase(key.substr(key.lastIndexOf('$') + 1))] = val
+            return assign(res, obj)
+          },
+          {}
+        )
+      },
+      context(req) {
+        return {
+          ...req,
+          ...services
+        }
+      }
+    })
+
+    return server
+  }
+}
+
+export default Server
