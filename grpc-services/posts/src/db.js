4736a7aeb155634579dc6ff431e92733404050b7
Benj Sicam <benj.sicam@mi-c3.com>
2019-07-21 23:22:06 +0800

    Added operator aliases to sequelize

--- grpc-services/posts/src/db.js
+++ grpc-services/posts/src/db.js
@@ -1,8 +1,46 @@
-import { Sequelize } from 'sequelize'
+import * as Sequelize from 'sequelize'
+
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+}
 
 const Db = {
   async init(modelPath, logger) {
-    const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    const db = new Sequelize.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       dialect: 'postgres',
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
@@ -11,7 +49,8 @@ const Db = {
       retry: {
         max: 3,
         typeValidation: true
-      }
+      },
+      operatorsAliases
     })
 
     db.import(modelPath)
