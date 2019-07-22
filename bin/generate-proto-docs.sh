9ba9adea636e7e3d27e3aba6341836c215005c55
Benj Sicam <benj.sicam@mi-c3.com>
2019-07-22 11:46:27 +0800

    Moved scripts to bin folder

--- bin/generate-proto-docs.sh
+++ bin/generate-proto-docs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd .. && \
 docker run --rm \
   -v $PWD/docs/proto:/out \
   -v $PWD/_proto:/protos \
-  pseudomuto/protoc-gen-doc --doc_opt=markdown,docs.md && \
-cd -
+  pseudomuto/protoc-gen-doc --doc_opt=markdown,docs.md
