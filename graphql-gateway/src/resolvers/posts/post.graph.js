1ddd607b5637fcf7af897be21549ae9a313682d2
achilles.dinglasan <achilles.dinglasan@mi-c3.com>
2019-10-04 15:07:54 +0800

    implemented sorting and pagination

--- graphql-gateway/src/resolvers/posts/post.graph.js
+++ graphql-gateway/src/resolvers/posts/post.graph.js
@@ -1,3 +1,6 @@
+import queryUtils from '../../utils/query'
+import { get } from 'lodash'
+
 const PostGraph = {
   async author(parent, args, { userService, logger }) {
     logger.info('PostGraph#author.call', parent.author)
@@ -13,12 +16,15 @@ const PostGraph = {
     return user
   },
   async comments(parent, args, { commentService, logger }) {
-    logger.info('PostGraph#comments.call', parent.id)
+    logger.info('PostGraph#comments.call', args)
 
     const comments = await commentService.findAll({
       where: {
         post: parent.id
-      }
+      },
+      limit: await queryUtils.setLimit(get(args, 'limit')),
+      offset: await queryUtils.setOffset(get(args, 'page'), get(args, 'limit') || 25),
+      order: await queryUtils.parseOrder(get(args, 'orderBy'))
     })
 
     logger.info('PostGraph#comments.result', comments)
