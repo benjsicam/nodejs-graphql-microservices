d4257f115af4b64affdfb52a47941b418b7a313f
Benj Sicam <benj.sicam@mi-c3.com>
2019-11-26 12:22:57 +0800

    Added scripts for working locally

---
+++ bin/build-all-local.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd graphql-gateway && npm i && npm run copy:protos && npm run compile && cd -
+cd grpc-services/comments && npm i && npm run copy:protos && npm run compile && cd -
+cd grpc-services/posts && npm i && npm run copy:protos && npm run compile && cd -
+cd grpc-services/users && npm i && npm run copy:protos && npm run compile && cd -
+docker-compose build --no-cache
--- bin/build-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd graphql-gateway && npm i && npm run copy:protos && npm run build && cd -
-cd grpc-services/comments && npm i && npm run copy:protos && npm run build && cd -
-cd grpc-services/posts && npm i && npm run copy:protos && npm run build && cd -
-cd grpc-services/users && npm i && npm run copy:protos && npm run build && cd -
-docker-compose build --no-cache
