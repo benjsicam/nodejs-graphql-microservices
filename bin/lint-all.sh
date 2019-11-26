5612a5f046ef1696b0574b413b29a2d7ebb15374
Benj Sicam <benj.sicam@mi-c3.com>
2019-11-26 12:23:44 +0800

    Added script for linting all sub-projects

---
+++ bin/lint-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd graphql-gateway && npm i && npm run lint && cd -
+cd grpc-services/comments && npm i && npm run lint && cd -
+cd grpc-services/posts && npm i && npm run lint && cd -
+cd grpc-services/users && npm i && npm run lint && cd -
