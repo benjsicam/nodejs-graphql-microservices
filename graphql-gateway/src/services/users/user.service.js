dd759ac2d4b8aaa0038c0a138c1ceedc2c414bbf
achilles.dinglasan <achilles.dinglasan@mi-c3.com>
2019-11-28 08:45:00 +0800

    refactored dataloader functions to use async/await format

--- graphql-gateway/src/services/users/user.service.js
+++ graphql-gateway/src/services/users/user.service.js
@@ -6,15 +6,16 @@ class UserService extends AbstractCrudService {
   constructor(client, logger) {
     super('UserService', client, logger)
 
-    this._loader = new Dataloader(keys => {
+    this._loader = new Dataloader(async (keys) => {
       this._logger.info(`Loading keys from ${this._serviceName}`, keys)
-      return this.findAll({
+      let rows = await this.findAll({
         where: {
           id: {
             $in: keys
           }
         }
-      }).then(rows => keys.map(key => rows.find(row => row.id === key)))
+      })
+      return keys.map(key => rows.find(row => row.id === key))
     })
   }
 
